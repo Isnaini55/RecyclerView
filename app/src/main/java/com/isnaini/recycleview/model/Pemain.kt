4596779d16def8baf7a394407f44f4f57789a0a0
Isnaini <isn.azn@gmail.com>
2022-07-07 20:23:43 +0700

    RecyclerView App

---
+++ app/src/main/java/com/isnaini/recycleview/model/Pemain.kt
@@ -0,0 +1,10 @@
+package com.isnaini.recycleview.model
+
+data class Pemain(
+    var nama : String,
+    var foto : Int,
+    var posisi : String,
+    var tinggibadan : String,
+    var tempatlahir : String,
+    var tanggallahir : String
+)
